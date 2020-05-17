da99889cf2c6dd84054e2a630143b2552c6030e0
Yiran Shao <charlottesyr@outlook.com>
2020-05-16 21:41:43 -0400

    Changed output format; Fixed bugs

--- Youtube.py
+++ Youtube.py
@@ -2,18 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from datetime import datetime
+import os
 
 
 listurl = 'https://www.youtube.com/playlist?list=PLiQ-AHFkVMznxM-NPbvtkVCQTR-8xXXI0'
 
-
+filename = str(datetime.now().date()).replace("-","")[2::]+".txt"
+f = open(filename, "a+")
+if os.path.exists(filename) and os.stat(filename).st_size != 0:
+    f.truncate(0)
 data = requests.get(listurl)
 data = data.text
 soup = BeautifulSoup(data,"html.parser")
 
 tr = soup.find_all("tr",class_="pl-video yt-uix-tile")
 
-for element in tr:
+for idx, element in enumerate(tr):
     info = element.text.strip().split("\n")
     info = [x for x in info if not (x.isspace()or x=="")]
     title = info[0]
@@ -35,8 +39,15 @@ for element in tr:
     else:
         caption = "无 提供mp3"
 
-    print("【"+channel+"】"+title+"（"+date+"）")
-    print(videourl)
-    print("### "+"\n"+"游戏：")
-    print("时长："+length)
-    print("CC："+ str(caption)+"\n")
+    print("加载到第"+str(idx+1)+"条视频: "+title)
+
+    f.write("【"+channel+"】"+title+"（"+date+"）"+"\n")
+    f.write(videourl+"\n")
+    f.write("### "+"\n"+
+            "游戏："+"\n"+
+            "时长："+length+"\n"+
+            "CC："+ str(caption)+"\n\n")
+    sumVideo = idx+1
+num_lines = sum(1 for line in f)
+print("文件生成完毕√")
+f.close()
